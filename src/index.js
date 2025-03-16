8289f5b371d3bb4ddad3472f3daed2a710f93569
Presbury <wamaepresbury034@gmail.com>
2025-03-16 17:37:12 +0300

    creates index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", ()=> {
+  
+
+const taskForm = document.getElementById('create-task-form');
+const newtaskdescription = document.getElementById('new-task-description');
+const taskul = document.getElementById('tasks');
+const button = document.querySelector('form input[type="submit"]')
+
+button.addEventListener('click',() => {
+  addTask(newtaskdescription.value);
+})
+
+taskForm.addEventListener('submit', function(event){
+  event.preventDefault();
+})
+
+function addTask(toDo){
+  let li = document.createElement("li");
+  li.textContent = `${toDo}`;
+  let deleteButton = document.createElement("button");
+  deleteButton.addEventListener('click', deleteTask)
+  deleteButton.textContent = "X";
+  li.appendChild(deleteButton);
+  newtaskdescription.value = "";
+  taskul.appendChild(li)
+}
+
+function deleteTask(event){
+  event.target.parentNode.remove()
+
+}
+
+})
